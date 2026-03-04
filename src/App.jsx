06ad0ea08c3f5f940e83d194b99ed3eaf37ac098
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-03-04 12:19:20 +0600

    data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 import Progress from "./Components/ProgressCard/Progress";
 import Resolved from "./Components/ResolvedCard/Resolved";
-
+import CustomerTickets from "./Components/CustomerTickets/CustomerTickets";
+const allTicketsPromise=fetch('/ticket-data.json').then((res)=>res.json())
 function App() {
   return (
     <>
@@ -13,14 +15,30 @@ function App() {
       <main className=" bg-[#f5f5f5]">
         {/* prgress and resolved section */}
         <section className="max-w-11/12 lg:max-w-10/12 mx-auto  px-1 lg:px-6 flex flex-col lg:flex-row items-center gap-6 py-20">
-        {/* progress card */}
-        <div className="lg:flex-1 w-full">
-          <Progress/>
-        </div>
-        {/* resolved card */}
-        <div className="lg:flex-1 w-full">
-          <Resolved/>
-        </div>
+          {/* progress card */}
+          <div className="lg:flex-1 w-full">
+            <Progress />
+          </div>
+          {/* resolved card */}
+          <div className="lg:flex-1 w-full">
+            <Resolved />
+          </div>
+        </section>
+
+
+
+
+        {/* customer support ticket layout */}
+        <section>
+          {/* all ticket showing customer problem section div */}
+          <div>
+            <Suspense fallback={<span className="loading loading-bars loading-xl"></span>}>
+              <CustomerTickets allTicketsPromise={allTicketsPromise}/>
+            </Suspense>
+          </div>
+
+          {/* right sidebar shwoing status section div */}
+          <div></div>
         </section>
       </main>
 
