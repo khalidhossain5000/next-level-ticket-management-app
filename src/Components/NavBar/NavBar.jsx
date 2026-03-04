78396ae7c61dace6ac3261980fc32d741074cda1
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-03-04 21:19:20 +0600

    mobile menu is updated

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -28,60 +28,64 @@ const NavBar = () => {
   ];
   return (
     <nav className="max-w-11/12 lg:max-w-10/12 mx-auto  px-1 lg:px-6 py-4 ">
-        <div className="flex items-center justify-between">
-
-      
-      {/* logo */}
-      <div>
-        <h2 className="text-[#130B2D] text-xl md:text-2xl font-bold">
-          CS — Ticket System
-        </h2>
-      </div>
-      {/* nav menu item and button */}
-      <div className="hidden xl:flex items-center gap-6">
-        <ul className="flex items-center gap-6">
-          {navItem.map((item, i) => (
-            <li key={i}>
-              <a href="#" className="text-[16px] text-[#000000]">
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        {/* button */}
+      <div className="flex items-center justify-between">
+        {/* logo */}
         <div>
-          <button className="px-4 py-3 rounded-sm flex items-center gap-3 text-white font-semibold text-[16px] bg-linear-to-br from-[#632ee3] to-[#9f62f2]">
-            <FaPlus />
-            New Ticket
-          </button>
+          <h2 className="text-[#130B2D] text-xl md:text-2xl font-bold">
+            CS — Ticket System
+          </h2>
+        </div>
+        {/* nav menu item and button */}
+        <div className="hidden xl:flex items-center gap-6">
+          <ul className="flex items-center gap-6">
+            {navItem.map((item, i) => (
+              <li key={i}>
+                <a href="#" className="text-[16px] text-[#000000]">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          {/* button */}
+          <div>
+            <button className="px-4 py-3 rounded-sm flex items-center gap-3 text-white font-semibold text-[16px] bg-linear-to-br from-[#632ee3] to-[#9f62f2]">
+              <FaPlus />
+              New Ticket
+            </button>
+          </div>
+        </div>
+
+        {/* mobile menu */}
+
+        <div className="xl:hidden">
+          {isOpen ? (
+            <button onClick={() => setIsOpen(false)}>
+              <span className="text-2xl">
+                <RxCross1 />
+              </span>
+            </button>
+          ) : (
+            <button onClick={() => setIsOpen(true)}>
+              <span className="text-2xl">
+                <GiHamburgerMenu />
+              </span>
+            </button>
+          )}
         </div>
       </div>
+      {/* menu items */}
 
-      {/* mobile menu */}
-
-      <div className="xl:hidden">
-        {isOpen ? (
-          <button onClick={()=>setIsOpen(false)}>
-            <span className="text-2xl">
-              <RxCross1 />
-            </span>
-          </button>
-        ) : (
-          <button onClick={()=>setIsOpen(true)}>
-            <span className="text-2xl">
-              <GiHamburgerMenu />
-            </span>
-          </button>
-        )}
-
-
-       
+      <div className="xl:hidden relative">
+        <div
+          className={`
+      absolute top-0 right-0 h-screen w-64 bg-slate-100 shadow-lg
+      transform transition-transform transition-opacity duration-300 ease-in-out
+      ${isOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}
+    `}
+        >
+          <MobileMenu navItem={navItem} />
+        </div>
       </div>
-        </div>
-       {/* menu items */}
-        <div className="xl:hidden relative">
-            {isOpen && <MobileMenu navItem={navItem}/>}
-        </div>
     </nav>
   );
 };
